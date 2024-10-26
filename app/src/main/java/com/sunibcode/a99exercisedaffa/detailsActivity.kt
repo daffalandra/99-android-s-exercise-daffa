153bb4a3a84561d05dd3d757cf70e13cdf15004f
Daffa Alandra <mdaf.alandra@gmail.com>
2024-10-26 23:34:44 +0700

    initial commit

---
+++ app/src/main/java/com/sunibcode/a99exercisedaffa/detailsActivity.kt
@@ -0,0 +1,20 @@
+package com.sunibcode.a99exercisedaffa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class detailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

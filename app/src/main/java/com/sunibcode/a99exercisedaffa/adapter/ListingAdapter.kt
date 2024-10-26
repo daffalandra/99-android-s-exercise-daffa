e86a7d0dfc50a1313b873a22f329463daba23530
Daffa Alandra <mdaf.alandra@gmail.com>
2024-10-27 00:00:51 +0700

    move ListingAdapter.kt to adapter directory

--- app/src/main/java/com/sunibcode/a99exercisedaffa/adapter/ListingAdapter.kt
+++ app/src/main/java/com/sunibcode/a99exercisedaffa/adapter/ListingAdapter.kt
@@ -1,4 +1,4 @@
-package com.sunibcode.a99exercisedaffa
+package com.sunibcode.a99exercisedaffa.adapter
 
 
 import android.view.LayoutInflater
@@ -8,6 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.sunibcode.a99exercisedaffa.R
 import com.sunibcode.a99exercisedaffa.model.Listing
 
 class ListingAdapter(private val listings: List<Listing>) :
